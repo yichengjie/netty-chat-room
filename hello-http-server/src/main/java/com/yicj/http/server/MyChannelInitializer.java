7966ced058ec63d5267b9d11b73c473cc2c92c7d
yichengjie <626659321@qq.com>
2023-07-16 20:39:45 +0800

    hello netty http server

--- hello-http-server/src/main/java/com/yicj/http/server/MyChannelInitializer.java
+++ hello-http-server/src/main/java/com/yicj/http/server/MyChannelInitializer.java
@@ -3,15 +3,17 @@ package com.yicj.http.server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
- 
+import io.netty.handler.codec.http.HttpServerCodec;
+
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
  
     protected void initChannel(SocketChannel ch) throws Exception {
         // 获取管道
         ChannelPipeline pipeline = ch.pipeline();
- 
+        HttpObjectAggregator httpObjectAggregator ;
         // 设置编码解码处理器
         pipeline.addLast("MyHttpResponseEncoder", new HttpResponseEncoder());
         pipeline.addLast("MyHttpRequestDecoder", new HttpRequestDecoder());
